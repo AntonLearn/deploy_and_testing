4511ddf3259de47950418369bd9fc94a8eacf068
AntonLearn <dj@cv4086443.novalocal>
2024-09-15 16:11:48 +0300

    cicd 13

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -28,12 +28,12 @@ SECRET_KEY = 'django-insecure-bl!k2l9r(hu+@5e0jzal7(0fc-5@-qdx^(9a2@%5&ifxc^nu3+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG', default='True') == 'True'
-DEBUG_STRING = os.environ.get('DEBUG')
-DEBUG = DEBUG_STRING == 'True'
-with open('output.txt', 'w') as f:
-    f.write('DEBUG_STRING = ' + repr(DEBUG_STRING) + '\n')
-    f.write('DEBUG = ' + repr(DEBUG))
-#DEBUG=False
+#DEBUG_STRING = os.environ.get('DEBUG')
+#DEBUG = DEBUG_STRING == 'True'
+#with open('output.txt', 'w') as f:
+#    f.write('DEBUG_STRING = ' + repr(DEBUG_STRING) + '\n')
+#    f.write('DEBUG = ' + repr(DEBUG))
+DEBUG = False
 #DEBUD=True
 
 ALLOWED_HOSTS = ["*"]
