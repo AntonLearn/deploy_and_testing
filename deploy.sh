65a16b1ad3e05eeced294d9762f8b250469cf370
AntonLearn <dj@cv4086443.novalocal>
2024-09-16 13:40:11 +0300

    cicd 20

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,4 @@ git pull origin master
 source env/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
-sudo systemctl stop gunicorn
-sudo systemctl daemon-reload
-sudo systemctl stop gunicorn
+sudo systemctl restart gunicorn
